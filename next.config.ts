4e1a4204ec7ee6c1fa3ae637808731a7f873b486
tran mai huong <tranmaihuong1907@gmail.com>
2025-11-06 01:09:01 +0700

    test fe

---
+++ next.config.ts
@@ -0,0 +1,32 @@
+import NextBundleAnalyzer from '@next/bundle-analyzer'
+import createNextIntlPlugin from 'next-intl/plugin'
+import type { NextConfig } from 'next'
+
+const withNextIntl = createNextIntlPlugin()
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'localhost',
+        pathname: '/**'
+      },
+      {
+        hostname: 'api-bigboy.duthanhduoc.com',
+        pathname: '/**'
+      },
+      {
+        hostname: 'via.placeholder.com',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+    ]
+  }
+}
+const withBundleAnalyzer = NextBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true'
+})
+export default withBundleAnalyzer(withNextIntl(nextConfig))
